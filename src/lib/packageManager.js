ec2db13fcdf7ce85fa7556d55ebae8c8212506d9
J1234D <100943748+J1234D@users.noreply.github.com>
2026-07-08 19:32:12 +0530

    fixed packageManager bug

--- src/lib/packageManager.js
+++ src/lib/packageManager.js
@@ -1,30 +1,37 @@
-const installed = new Set(JSON.parse(localStorage.getItem("packages") ?? "[]"));
+function getInstalled() {
+  return new Set(JSON.parse(localStorage.getItem("packages") ?? "[]"));
+}
 
 async function loadInstalled(pyodide) {
-  for (const pkg of installed) {
-    try {
+  const installed = getInstalled();
+
+  try {
+    for (const pkg of installed) {
       await pyodide.loadPackage(pkg);
-      return "Installed packages are loaded";
-    } catch (err) {
-      return err.message;
     }
-    
+    return "Installed packages are loaded";
+  } catch (err) {
+    return err.message;
   }
 }
+
 async function installPackage(name, pyodide) {
+  const installed = getInstalled();
+
   if (!installed.has(name)) {
     try {
       await pyodide.loadPackage(name);
+
       installed.add(name);
       localStorage.setItem("packages", JSON.stringify([...installed]));
+
       return "Package installed";
     } catch (err) {
       return err.message;
     }
   }
-  else{
-    return("Package was already installed")
-  }
+
+  return "Package was already installed";
 }
 
 export default { loadInstalled, installPackage };
